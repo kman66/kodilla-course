6a043bd682ea96d6baabba5e5b2cbce04400b7ed
kman66 <karol.manterys@gmail.com>
2018-05-21 22:11:55 +0200

    giving proper name for test method and using stream instead of for each loop

--- kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
@@ -1,15 +1,16 @@
 package com.kodilla.stream.forumuser;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class Forum {
     private final List<ForumUser> list = new ArrayList<>();
 
     public void addForumUser(final ForumUser ... forumUsers){
-        for(ForumUser f : forumUsers){
-            list.add(f);
-        }
+        list.addAll(Arrays.stream(forumUsers).collect(Collectors.toList()));
     }
 
     public List<ForumUser> getList(){
