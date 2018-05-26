5f281728d14eb151a73df47f6d6d08a6e7466f45
kman66 <karol.manterys@gmail.com>
2018-05-26 14:29:33 +0200

    adding new class 'TestBoardHelper' which generate test variables

--- kodilla-stream/src/test/java/com/kodilla/stream/portfolio/BoardTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/portfolio/BoardTestSuite.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream.portfolio;
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.time.LocalDate;
@@ -13,72 +14,17 @@ import java.util.stream.IntStream;
 import static java.util.stream.Collectors.toList;
 
 public class BoardTestSuite {
-    private Board prepareTestData() {
-        //users
-        User user1 = new User("developer1", "John Smith");
-        User user2 = new User("projectmanager1", "Nina White");
-        User user3 = new User("developer2", "Emilia Stephanson");
-        User user4 = new User("developer3", "Konrad Bridge");
-        //tasks
-        Task task1 = new Task("Microservice for taking temperature",
-                "Write and test the microservice taking\n" +
-                        "the temperaure from external service",
-                user1,
-                user2,
-                LocalDate.now().minusDays(20),
-                LocalDate.now().plusDays(30));
-        Task task2 = new Task("HQLs for analysis",
-                "Prepare some HQL queries for analysis",
-                user1,
-                user2,
-                LocalDate.now().minusDays(20),
-                LocalDate.now().minusDays(5));
-        Task task3 = new Task("Temperatures entity",
-                "Prepare entity for temperatures",
-                user3,
-                user2,
-                LocalDate.now().minusDays(20),
-                LocalDate.now().plusDays(15));
-        Task task4 = new Task("Own logger",
-                "Refactor company logger to meet our needs",
-                user3,
-                user2,
-                LocalDate.now().minusDays(10),
-                LocalDate.now().plusDays(25));
-        Task task5 = new Task("Optimize searching",
-                "Archive data searching has to be optimized",
-                user4,
-                user2,
-                LocalDate.now(),
-                LocalDate.now().plusDays(5));
-        Task task6 = new Task("Use Streams",
-                "use Streams rather than for-loops in predictions",
-                user4,
-                user2,
-                LocalDate.now().minusDays(15),
-                LocalDate.now().minusDays(2));
-        //taskLists
-        TaskList taskListToDo = new TaskList("To do");
-        taskListToDo.addTask(task1);
-        taskListToDo.addTask(task3);
-        TaskList taskListInProgress = new TaskList("In progress");
-        taskListInProgress.addTask(task5);
-        taskListInProgress.addTask(task4);
-        taskListInProgress.addTask(task2);
-        TaskList taskListDone = new TaskList("Done");
-        taskListDone.addTask(task6);
-        //board
-        Board project = new Board("Project Weather Prediction");
-        project.addTaskList(taskListToDo);
-        project.addTaskList(taskListInProgress);
-        project.addTaskList(taskListDone);
-        return project;
+    private static TestBoardHelper testBoardHelper;
+
+    @BeforeClass
+    public static void createBoardBeforeAllTests(){
+        testBoardHelper = new TestBoardHelper();
     }
 
     @Test
     public void testAddTaskList() {
         //Given
-        Board project = prepareTestData();
+        Board project = testBoardHelper.prepareTestData();
         //When
 
         //Then
@@ -88,7 +34,7 @@ public class BoardTestSuite {
     @Test
     public void testAddTaskListFindUsersTasks() {
         //Given
-        Board project = prepareTestData();
+        Board project = testBoardHelper.prepareTestData();
         //When
         User user = new User("developer1", "John Smith");
         List<Task> tasks = project.getTaskLists().stream()
@@ -104,7 +50,7 @@ public class BoardTestSuite {
     @Test
     public void testAddTaskListFindOutdatedTasks() {
         //Given
-        Board project = prepareTestData();
+        Board project = testBoardHelper.prepareTestData();
 
         //When
         List<TaskList> undoneTasks = new ArrayList<>();
@@ -124,7 +70,7 @@ public class BoardTestSuite {
     @Test
     public void testAddTaskListFindLongTasks() {
         //Given
-        Board project = prepareTestData();
+        Board project = testBoardHelper.prepareTestData();
 
         //When
         List<TaskList> inProgressTasks = new ArrayList<>();
@@ -144,7 +90,7 @@ public class BoardTestSuite {
     @Test
     public void testAddTaskListAverageWorkingOnTask(){
         //Given
-        Board project = prepareTestData();
+        Board project = testBoardHelper.prepareTestData();
 
         //When
         List<TaskList> inProgressTasks = new ArrayList<>();
