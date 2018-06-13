8df7747749035abb84d29c1346f25c9d0c69d06a
kman66 <karol.manterys@gmail.com>
2018-06-13 22:47:09 +0200

    adding new test for Logger for case if more than one log

--- kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 public class LoggerTestSuite {
     @Test
-    public void testGetLastLog() {
+    public void testGetLastLogIfOnlyOne() {
         //Given
         Logger logger = Logger.getInstance();
         logger.log("test log");
@@ -15,5 +15,20 @@ public class LoggerTestSuite {
 
         //Then
         Assert.assertEquals("test log", result);
+    }
+
+    @Test
+    public void testGetLastLogIfMoreThanOne() {
+        //Given
+        Logger logger = Logger.getInstance();
+        logger.log("first log");
+        logger.log("second log");
+        logger.log("third log");
+
+        //When
+        String result = logger.getLastLog();
+
+        //Then
+        Assert.assertEquals("third log", result);
     }
 }
