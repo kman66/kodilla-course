0baa8bc35a6e605637739e0a47b6629365eae56c
kman66 <karol.manterys@gmail.com>
2018-06-16 18:01:24 +0200

    executing submodule 11.4

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/factory/tasks/PaintingTask.java
@@ -0,0 +1,31 @@
+package com.kodilla.patterns.factory.tasks;
+
+public final class PaintingTask implements Task {
+    final private String taskName;
+    final private String color;
+    final private String whatToPaint;
+    private boolean isExecuted;
+
+    public PaintingTask(final String taskName, final String color, final String whatToPaint) {
+        this.taskName = taskName;
+        this.color = color;
+        this.whatToPaint = whatToPaint;
+        isExecuted = false;
+    }
+
+    @Override
+    public void executeTask() {
+        System.out.println("Executing painting task '" + taskName);
+        isExecuted = true;
+    }
+
+    @Override
+    public String getTaskName() {
+        return taskName;
+    }
+
+    @Override
+    public boolean isTaskExecuted() {
+        return isExecuted;
+    }
+}
---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/factory/tasks/TaskTypes.java
@@ -0,0 +1,7 @@
+package com.kodilla.patterns.factory.tasks;
+
+public class TaskTypes {
+    public static final String DRIVING = "DRIVING";
+    public static final String SHOPPING = "SHOPPING";
+    public static final String PAINTING = "PAINTING";
+}
